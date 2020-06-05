eb7089ad3c17e1a91e90242fb1e18b620a7172d9
stefan <2572915286@qq.com>
2020-06-05 15:34:01 +0800

    fix webnet.

--- akWebNet/server.go
+++ akWebNet/server.go
@@ -5,25 +5,34 @@ package akWebNet
 //
 
 import (
+	"context"
 	"fmt"
 	"net/http"
+	"os"
+	"os/signal"
 	"sync"
+	"syscall"
 
+	"github.com/Peakchen/xgameCommon/akLog"
 	"github.com/Peakchen/xgameCommon/aktime"
+	"github.com/Peakchen/xgameCommon/pprof"
 	"github.com/gorilla/websocket"
 	//"strings"
 	//"strconv"
 )
 
 type WebSocketSvr struct {
-	Addr  string
-	offch chan *WebSession //离线通道
+	Addr      string
+	pprofAddr string
+	offch     chan *WebSession //离线通道
+	cancel    context.CancelFunc
 }
 
-func NewWebsocketSvr(addr string) *WebSocketSvr {
+func NewWebsocketSvr(addr string, pprofAddr string) *WebSocketSvr {
 	return &WebSocketSvr{
-		Addr:  addr,
-		offch: make(chan *WebSession, 1024),
+		Addr:      addr,
+		offch:     make(chan *WebSession, 1024),
+		pprofAddr: pprofAddr,
 	}
 }
 
@@ -49,23 +58,63 @@ func (this *WebSocketSvr) wsSvrHandler(resp http.ResponseWriter, req *http.Reque
 	GwebSessionMgr.AddSession(sess)
 }
 
-func (this *WebSocketSvr) disconnloop() {
+func (this *WebSocketSvr) disconnloop(ctx context.Context, sw *sync.WaitGroup) {
+	defer func() {
+		sw.Done()
+		this.exit()
+	}()
+
 	for {
-		sess := <-this.offch
-		mosterid := sess.GetId()
-		fmt.Println("exit ws socket: ", sess.RemoteAddr, mosterid, aktime.Now().Unix())
-		GwebSessionMgr.RemoveSession(sess.RemoteAddr)
-		if proc := GetGameLogicProcMsg(MID_logout); proc != nil {
-			proc(sess, []uint32{mosterid})
+		select {
+		case sess := <-this.offch:
+			mosterid := sess.GetId()
+			fmt.Println("exit ws socket: ", sess.RemoteAddr, mosterid, aktime.Now().Unix())
+			GwebSessionMgr.RemoveSession(sess.RemoteAddr)
+			if proc := GetGameLogicProcMsg(MID_logout); proc != nil {
+				proc(sess, []uint32{mosterid})
+			}
+		case <-ctx.Done():
+			return
 		}
 	}
 }
 
 func (this *WebSocketSvr) Run() {
-
 	http.HandleFunc("/ws", this.wsSvrHandler)
+	var ctx context.Context
+	ctx, this.cancel = context.WithCancel(context.Background())
+	pprof.Run(ctx)
 	var sw sync.WaitGroup
 	sw.Add(1)
-	go this.disconnloop()
+	go this.disconnloop(ctx, &sw)
+	go this.loopSignalCheck(ctx, &sw)
 	sw.Wait()
+}
+
+func (this *WebSocketSvr) exit() {
+	this.cancel()
+}
+
+func (this *WebSocketSvr) loopSignalCheck(ctx context.Context, sw *sync.WaitGroup) {
+	defer func() {
+		sw.Done()
+		this.exit()
+	}()
+
+	chsignal := make(chan os.Signal, 1)
+	signal.Notify(chsignal, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case s := <-chsignal:
+			switch s {
+			case syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT:
+				akLog.FmtPrintln("signal exit:", s)
+				return
+			default:
+				akLog.FmtPrintln("other signal:", s)
+			}
+		}
+	}
 }
